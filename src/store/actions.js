d203cbffeb005fa0c9bc4ce7c1c418b1087e1ed5
ItsMos <mospro@live.com>
2022-06-15 13:00:10 +0200

    * split vuex into files

---
+++ src/store/actions.js
@@ -0,0 +1,118 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import { findById, docToResource } from '@/helpers'
+
+export default {
+  async createPost({ commit, state }, post) {
+    post.userId = state.authId
+    post.publishedAt = firebase.firestore.FieldValue.serverTimestamp()
+
+    const batch = firebase.firestore().batch()
+    const postRef = firebase.firestore().collection('posts').doc()
+    const threadRef = firebase.firestore().collection('threads').doc(post.threadId)
+    const userRef = firebase.firestore().collection('users').doc(state.authId)
+    batch.set(postRef, post)
+    batch.update(threadRef, {
+      posts: firebase.firestore.FieldValue.arrayUnion(postRef.id),
+      contributors: firebase.firestore.FieldValue.arrayUnion(state.authId)
+    })
+    batch.update(userRef, {
+      postsCount: firebase.firestore.FieldValue.increment(1)
+    })
+    await batch.commit()
+    const newPost = await postRef.get()
+    commit('setItem', { resource: 'posts', item: { ...newPost.data(), id: newPost.id } })
+    commit('appendPostToThread', { childId: newPost.id, parentId: post.threadId })
+    commit('appendContributorToThread', { childId: state.authUser, parentId: post.threadId })
+  },
+
+  async createThread({ commit, state, dispatch }, { title, text, forumId }) {
+    const userId = state.authId
+    const publishedAt = firebase.firestore.FieldValue.serverTimestamp()
+    const threadRef = firebase.firestore().collection('threads').doc()
+    const thread = { forumId, title, publishedAt, userId, id: threadRef.id }
+    const forumRef = firebase.firestore().collection('forums').doc(forumId)
+    const userRef = firebase.firestore().collection('users').doc(userId)
+    const batch = firebase.firestore().batch()
+
+    batch.set(threadRef, thread)
+    batch.update(userRef, {
+      threads: firebase.firestore.FieldValue.arrayUnion(threadRef.id)
+    })
+    batch.update(forumRef, {
+      threads: firebase.firestore.FieldValue.arrayUnion(threadRef.id)
+    })
+    await batch.commit()
+    const newThread = await threadRef.get()
+
+    commit('setItem', { resource: 'threads', item: { ...newThread.data(), id: newThread.id } })
+    commit('appendThreadToForum', { childId: threadRef.id, parentId: forumId })
+    commit('appendThreadToUser', { childId: threadRef.id, parentId: userId })
+    await dispatch('createPost', { text, threadId: threadRef.id })
+    return findById(state.threads, threadRef.id)
+  },
+
+  async updateThread({ commit, state }, { title, text, id }) {
+    const thread = findById(state.threads, id)
+    const post = findById(state.posts, thread.posts[0])
+    let newThread = { ...thread, title }
+    let newPost = { ...post, text }
+    const threadRef = firebase.firestore().collection('threads').doc(id)
+    const postRef = firebase.firestore().collection('posts').doc(post.id)
+    const batch = firebase.firestore().batch()
+    batch.update(threadRef, newThread)
+    batch.update(postRef, newPost)
+    await batch.commit()
+    newThread = await threadRef.get()
+    newPost = await postRef.get()
+    commit('setItem', { resource: 'threads', item: newThread })
+    commit('setItem', { resource: 'posts', item: newPost })
+    return docToResource(newThread)
+  },
+
+  updateUser({ commit }, user) {
+    commit('setItem', { resource: 'users', item: user, userId: user.id })
+  },
+
+  fetchCategory: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'categories', id }),
+  fetchForum: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'forums', id }),
+  fetchThread: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'threads', id }),
+  fetchPost: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'posts', id }),
+  fetchUser: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'users', id }),
+  fetchAuthUser: ({ dispatch, state }) => dispatch('fetchItem', { resource: 'users', id: state.authId }),
+
+  fetchAllCategories({ commit }) {
+    console.log('fetching all categories')
+    return new Promise(resolve => {
+      firebase.firestore().collection('categories').onSnapshot((querySnapshot) => {
+        const categories = querySnapshot.docs.map(doc => {
+          const item = { id: doc.id, ...doc.data() }
+          commit('setItem', { resource: 'categories', item })
+          return item
+        })
+        resolve(categories)
+      })
+    })
+  },
+
+  fetchCategories: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'categories', ids }),
+  fetchForums: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'forums', ids }),
+  fetchThreads: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'threads', ids }),
+  fetchPosts: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'posts', ids }),
+  fetchUsers: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'users', ids }),
+
+  fetchItem({ commit }, { id, resource }) {
+    console.log('fetching from ' + resource, id)
+    return new Promise((resolve) => {
+      firebase.firestore().collection(resource).doc(id).onSnapshot(doc => {
+        const item = { ...doc.data(), id: doc.id }
+        commit('setItem', { resource, id, item })
+        resolve(item)
+      })
+    })
+  },
+
+  fetchItems({ dispatch }, { ids, resource }) {
+    return Promise.all(ids.map(id => dispatch('fetchItem', { id, resource })))
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from 'vuex'
-import { findById, upsert } from '@/helpers'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
+import actions from '@/store/actions'
+import getters from '@/store/getters'
+import mutations from '@/store/mutations'
 
 export default createStore({
   state: {
     categories: [],
-    forms: [],
+    forums: [],
     threads: [],
     posts: [],
     users: [],
@@ -15,133 +15,7 @@ export default createStore({
     // authId: 'u4r8XCziZEWEXsj2UIKNHBoDh0n2'
   },
 
-  getters: {
-    authUser: (state, getters) => {
-      return getters.user(state.authId)
-    },
-
-    user: state => {
-      return id => {
-        const user = findById(state.users, id)
-        if (!user) return null
-        return {
-          ...user,
-          get posts() {
-            return state.posts.filter(p => p.userId === user.id)
-          },
-
-          get postsCount() {
-            return this.posts.length
-          },
-
-          get threads() {
-            return state.threads.filter(th => th.userId === user.id)
-          },
-
-          get threadsCount() {
-            return this.threads.length
-          }
-        }
-      }
-    },
-
-    thread: state => {
-      return id => {
-        const thread = findById(state.threads, id)
-        return {
-          ...thread,
-          get author() {
-            return findById(state.users, thread.userId)
-          },
-          get repliesCount() {
-            return thread.posts.length - 1
-          },
-          get contributorsCount() {
-            return thread?.contributors?.length || 0
-          }
-        }
-      }
-    }
-  },
-
-  actions: {
-    createPost({ commit, state }, post) {
-      post.id = 'po' + Math.random()
-      post.userId = state.authId
-      post.publishedAt = Math.floor(Date.now() / 1000)
-      commit('setItem', { resource: 'posts', post })
-      commit('appendPostToThread', { childId: post.id, parentId: post.threadId })
-      commit('appendContributorToThread', { childId: state.authUser, parentId: post.threadId })
-    },
-
-    async createThread({ commit, state, dispatch }, { title, text, forumId }) {
-      const id = 'th' + Math.random()
-      const userId = state.authId
-      const publishedAt = Math.floor(Date.now() / 1000)
-      const thread = { forumId, title, publishedAt, userId, id }
-      commit('setItem', { resource: 'threads', thread })
-      commit('appendThreadToForum', { childId: id, parentId: forumId })
-      commit('appendThreadToUser', { childId: id, parentId: userId })
-      dispatch('createPost', { text, threadId: id })
-      return findById(state.threads, id)
-    },
-
-    async updateThread({ commit, state }, { title, text, id }) {
-      const thread = findById(state.threads, id)
-      const post = findById(state.posts, thread.posts[0])
-      const newThread = { ...thread, title }
-      const newPost = { ...post, text }
-      commit('setItem', { resource: 'threads', newThread })
-      commit('setItem', { resource: 'posts', post: newPost })
-      return newThread
-    },
-
-    updateUser({ commit }, user) {
-      commit('setItem', { resource: 'users', user, userId: user.id })
-    },
-
-    fetchThread({ dispatch }, { id }) {
-      return dispatch('fetchItem', { resource: 'threads', id })
-    },
-
-    fetchUser({ dispatch }, { id }) {
-      return dispatch('fetchItem', { resource: 'users', id })
-    },
-
-    fetchPost({ dispatch }, { id }) {
-      return dispatch('fetchItem', { resource: 'posts', id })
-    },
-
-    fetchItem({ commit }, { id, resource }) {
-      console.log('fetching from ' + resource, id)
-      return new Promise((resolve) => {
-        firebase.firestore().collection(resource).doc(id).onSnapshot(doc => {
-          const item = { ...doc.data(), id: doc.id }
-          commit('setItem', { resource, id, item })
-          resolve(item)
-        })
-      })
-    }
-  },
-
-  mutations: {
-    setItem(state, { resource, item }) {
-      upsert(state[resource], item)
-    },
-
-    appendPostToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'posts' }),
-    appendThreadToForum: makeAppendChildToParentMutation({ parent: 'forums', child: 'threads' }),
-    appendThreadToUser: makeAppendChildToParentMutation({ parent: 'users', child: 'threads' }),
-    appendContributorToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'contributors' })
-  }
+  getters,
+  actions,
+  mutations
 })
-
-function makeAppendChildToParentMutation({ parent, child }) {
-  return (state, { childId, parentId }) => {
-    const resource = findById(state[parent], parentId)
-    resource[child] = resource[child] || []
-    if (!resource[child].includes(childId)) {
-      resource[child].push(childId)
-    }
-  }
-}
