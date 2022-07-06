d2211d1c68d71a337d3543eb367259b653572e13
ItsMos <mospro@live.com>
2022-07-07 01:03:55 +0200

    + edit posts

--- src/components/postEditor.vue
+++ src/components/postEditor.vue
@@ -5,7 +5,7 @@
         <textarea v-model="text" name="" id="" cols="30" rows="10" class="form-input"></textarea>
       </div>
       <div class="form">
-        <button class="btn-blue">Submit post</button>
+        <button class="btn-blue">{{post ? 'Update Post' : 'Submit post'}}</button>
       </div>
     </form>
   </div>
@@ -13,20 +13,29 @@
 
 <script>
 export default {
+  props: {
+    post: {
+      type: Object
+    }
+  },
+
   data() {
     return {
-      text: ''
+      text: this.post?.text || ''
     }
   },
 
   methods: {
     save() {
       if (this.text.length < 2) return
-      const post = {
-        text: this.text
+      let post = { text: this.text }
+      if (this.post) {
+        post = { ...this.post, text: this.text }
       }
       this.$emit('save', { post })
-      this.text = ''
+      if (!this.post) {
+        this.text = ''
+      }
     }
   }
 }
